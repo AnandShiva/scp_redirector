0ef4456c2b91bd7beb46b1aa2f7cba5978dd8ac5
Anandkumar S <anandkumar.s@sap.com>
2019-06-16 16:54:40 +0530

    Boot and base project setup

---
+++ src/main/java/redirector/AppInitializer.java
@@ -0,0 +1,13 @@
+package redirector;
+
+import java.util.Arrays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class AppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(AppInitializer.class, args);
+    }
+}
