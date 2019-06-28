6d38d7921a62f9c0c4901f6c9a358b2baa5eb6ce
Anandkumar S <anandkumar.s@sap.com>
2019-06-28 15:35:39 +0530

    Added Auth dependencies for SCP

--- src/main/java/redirector/HelloController.java
+++ src/main/java/redirector/HelloController.java
@@ -1,14 +1,19 @@
 package redirector;
 
+import redirector.auth.*;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URISyntaxException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/destinations")
-    public String getDestinations() {
-        return "List of destinations ! ";
+    public String getDestinations() throws URISyntaxException {
+        //AuthorizationManager authManager = new AuthorizationManager("", "", "");
+        return "List of destinations ! ";//+authManager.getAuthTokenString();
     }
 
 }
--- src/main/java/redirector/auth/AuthorizationManager.java
+++ src/main/java/redirector/auth/AuthorizationManager.java
@@ -1,31 +1,41 @@
 package redirector.auth;
 
+import org.cloudfoundry.identity.client.UaaContext;
+import org.cloudfoundry.identity.client.UaaContextFactory;
+import org.cloudfoundry.identity.client.token.GrantType;
+import org.cloudfoundry.identity.client.token.TokenRequest;
+import org.cloudfoundry.identity.uaa.oauth.token.CompositeAccessToken;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 
-class AuthorizationManager {
+public class AuthorizationManager {
     private String userId;
     private String password;
-    private URI xsuaaUrl ;
+    private URI xsuaaUrl;
+    CompositeAccessToken accessToken;
 
     public AuthorizationManager(String userId, String password, String url) throws URISyntaxException {
         this.userId = userId;
         this.password = password;
         // URL section of XSUAA env variable.
-        this.xsuaaUrl  = new URI(url);
+        this.xsuaaUrl = new URI(url);
 
         this.userId = "sb-clone889a10d6bf704c69bd61afe7c9adbb78!b6037|destination-xsappname!b433";
         this.password = "9wfsV7MtKwKjGXayumt1//zg6o0=";
         this.xsuaaUrl = new URI("https://i332878trial.authentication.sap.hana.ondemand.com");
-    }
 
-    private String fetchAuthToken() {
-        String var = "";
-        return "";
-
+        // Make request to UAA to retrieve JWT
+        UaaContextFactory factory = UaaContextFactory.factory(xsuaaUrl);
+        TokenRequest tokenRequest = factory.tokenRequest();
+        tokenRequest.setGrantType(GrantType.CLIENT_CREDENTIALS);
+        tokenRequest.setClientId(this.userId);
+        tokenRequest.setClientSecret(this.password);
+        UaaContext xsUaaContext = factory.authenticate(tokenRequest);
+        this.accessToken = xsUaaContext.getToken();
     }
 
     public String getAuthTokenString(){
-        return fetchAuthToken();
+        return this.accessToken.getValue();
     }
 }
